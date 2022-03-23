2b3f189adc8e58a19e69ad53fcc14aa4b2220032
Sm1Ly <sm1lyprogrammer@gmail.com>
2022-03-23 15:04:33 +0100

    load correct file in resource manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,4 +5,4 @@ author 'Sm1Ly'
 description 'A resource create api calls for external admin panels, companion apps, ect.'
 version '1.0.0'
 
-server_script "server/server.lua"
+server_script "server/main.lua"
