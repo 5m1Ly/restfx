7ee034e1203d74cc87c0e661f24e10e70b10b71d
Sm1Ly <sm1lyprogrammer@gmail.com>
2022-03-24 13:33:43 +0100

    set split function back to original place

--- server/api/routers/4_handler.lua
+++ server/api/routers/4_handler.lua
@@ -22,6 +22,20 @@ end
 
 function Router:handler(req, res)
 
+	local function split(str, delimiter)
+		if str == nil then return end
+		local result = {}
+		local from = 1
+		local delim_from, delim_to = string.find(str, delimiter, from)
+		while delim_from do
+			result[#result+1] = string.sub(str, from, delim_from - 1)
+			from = delim_to + 1
+			delim_from, delim_to = string.find(str, delimiter, from)
+		end
+		result[#result+1] = string.sub(str, from)
+		return result
+	end
+
 	local Response = Res.new(res)
 
 	-- remove the first dash
