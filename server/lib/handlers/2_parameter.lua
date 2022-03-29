8d6abbdfaae1302738f1174520d369e24a05ca85
5m1Ly <sm1lyprogrammer@gmail.com>
2022-03-29 15:11:25 +0200

    fixed override

--- server/lib/handlers/2_parameter.lua
+++ server/lib/handlers/2_parameter.lua
@@ -7,7 +7,7 @@ function Parameter.new()
 		__index = Parameter,
 		__call = function(self, name, handler, bool)
 			local param = self.global[name]
-			if param == nil or (param ~= nil and bool) then
+			if param == nil or (bool and param ~= nil) then
 				self.global[name] = handler
 			else
 				error('the parameter you tried to create a handler for all ready exists', 0)
