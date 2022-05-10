a234ac111e293a00af7dfc8852ad8892bbfc06cb
5m1Ly <hbouma01@gmail.com>
2022-05-10 11:58:42 +0200

    final v2 commit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'rdr3', 'gta5' }
 
 author 'Sm1Ly'
 description 'A resource you can use to make api calls to you server'
-version '1.6.0'
+version '2.0.0'
 stay_up_to_date 'true'
 
 server_scripts {
